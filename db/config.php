103e0c4d0f8e5847ca82c8376c35f808c5ff409d
vedharaj <crisvedharaj@gmail.com>
2026-03-12 21:19:03 +0530

    MCAWEB25-20 Refactor database connection settings in config.php to use local environment

--- db/config.php
+++ db/config.php
@@ -1,16 +1,11 @@
 <?php
 
-$host = "sql309.infinityfree.com";
-$user = "if0_41372192";
-$password = "FsAlYWzzvqg";
-$database = "if0_41372192_placement";
+$conn = mysqli_connect("localhost","root","","placement");
 
-$conn = mysqli_connect($host, $user, $password, $database);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if($conn){
+    // echo "Database connected!";
+}else{
+    echo "Connection failed";
 }
-
-echo "Connected successfully";
 
 ?>
