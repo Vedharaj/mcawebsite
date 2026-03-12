9def7b45b9f9942b419832009a64996e386b5ed3
vedharaj <crisvedharaj@gmail.com>
2026-03-12 23:39:58 +0530

    MCAWEB25-14 reusable navbar

---
+++ components/navbar.php
@@ -0,0 +1,15 @@
+<nav class="bg-blue-900 text-white px-8 py-4 shadow">
+<div class="flex justify-between items-center">
+<h1 class="text-xl font-bold">
+<i class="fa-solid fa-graduation-cap mr-2"></i>
+MCA Department
+</h1>
+
+<ul class="flex gap-6 text-sm">
+<li class="hover:text-gray-200 cursor-pointer">Home</li>
+<li class="hover:text-gray-200 cursor-pointer">About</li>
+<li class="hover:text-gray-200 cursor-pointer">History</li>
+<li class="hover:text-gray-200 cursor-pointer">Contact</li>
+</ul>
+</div>
+</nav>
